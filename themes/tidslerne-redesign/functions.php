45f668e9bf6b9fa4ea32aa3256f9a42afbf42bea
EisnerMarko <mareis01@easv365.dk>
2025-05-27 13:14:52 +0200

    del adjustment

--- themes/tidslerne-redesign/functions.php
+++ themes/tidslerne-redesign/functions.php
@@ -3,10 +3,6 @@ function enqueue_theme_assets() {
     wp_enqueue_style('styles', get_template_directory_uri() . '/css/style.css', array(), filemtime(get_template_directory() . '/css/style.css'));
     wp_enqueue_script('menuScripts', get_template_directory_uri() . '/js/menuScripts.js', [], null, true);
 
-    if (is_singular('gallery')) {
-        wp_enqueue_script('img-gallery', get_template_directory_uri() . '/js/imgGallery.js', array(), filemtime(get_template_directory() . '/js/imgGallery.js'), true);
-    }
-
     if (is_category() || is_page('category')) {
         wp_enqueue_script('category-filter', get_template_directory_uri() . '/js/categoryFilter.js', array(), filemtime(get_template_directory() . '/js/categoryFilter.js'), true);
     }
